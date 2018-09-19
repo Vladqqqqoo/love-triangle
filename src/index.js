085770b5b8d2f675343d8e66164970c3bbc460b0
Vladislav Drobovich <vladdrobovich@gmail.com>
2018-09-19 16:12:49 +0300

    gogo next

--- src/index.js
+++ src/index.js
@@ -3,5 +3,34 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+	// your implementation
+	var a=0;
+	var b=0;
+	var c=0;
+	var count=0;
+	for (var i=0;i<preferences.length;i++){
+		a=preferences[i];
+		if(a==0 || a==(i+1)){
+		continue;
+		}
+		b=preferences[a-1];
+		if(b==0 || b==a){
+		continue;
+		}
+		c=preferences[b-1];
+		if(c==0 || c==b){
+		continue;
+	}
+		if(a==b && a==c && b==c){
+			continue;
+		}
+		if(c==(i+1))
+		{
+			count++;
+			preferences[i]=0;
+			preferences[a-1]=0;
+			preferences[b-1]=0;
+		}
+	}
+	return count;
 };
